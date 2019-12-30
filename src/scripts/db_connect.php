d0d910b6d47acda498615961491b333a0ca38a93
PetriskoRastislav <petrisko.rastislav@gmail.com>
2019-12-30 18:25:37 +0100

    Changed message in thrown Exception in db_connect.php

--- src/scripts/db_connect.php
+++ src/scripts/db_connect.php
@@ -4,7 +4,7 @@
 function db_connect() {
     $result = new mysqli('localhost', 'WebUser', 'SN13wEb19-20', 'SocialNetwork');
     if (!$result) {
-        throw new Exception('Nepodařilo se připojit k databázi.');
+        throw new Exception('Failed to connect to database.');
     } else {
         return $result;
     }
