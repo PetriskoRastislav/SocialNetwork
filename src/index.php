3d6e4d428f55322aa76c8052a043bf3b1043ff45
PetriskoRastislav <petrisko.rastislav@gmail.com>
2019-12-30 20:15:23 +0100

    fixed error in registration

--- src/index.php
+++ src/index.php
@@ -1,9 +1,13 @@
 <?php
+    require_once('scripts/scripts.php');
+
+    session_start();
+
     if(isset($_SESSION['id_user'])){
         header('Location: user.php');
         exit();
     }
-    require_once('scripts/scripts.php');
+
     $page = new Page();
     $page->displayHead( "login", array("styles/style_form.css", "styles/style_form-dark.css"));
     $page->displayBodyStart();
@@ -14,7 +18,7 @@
     <button onclick="showLogForm()" class="logreg_button logreg_active" id="log_button">Prihlásenie</button>
     <button onclick="showRegForm()" class="logreg_button" id="reg_button">Registrácia</button>
 
-    <form method="POST" class="logreg" id="registration" action="scripts/registration.php">
+    <form method="POST" class="logreg" id="registration">
 
         <p class="spacing_form">
             <label for="name_reg">
@@ -57,14 +61,14 @@
         <input required class="spacing_form input_form" type="password" name="password2" id="password2_reg" size="35" placeholder="heslo znovu" />
 
         <p class="spacing_form">
-            <button class="spacing_form button_form_submit" type="submit" form="registration" formaction="">Zaregistrovať</button>
+            <button class="spacing_form button_form_submit" type="submit" form="registration" formaction="scripts/registration.php">Zaregistrovať</button>
         </p>
 
         <p class="spacing_form"><span class="required">*</span> Povinné pole.</p>
 
     </form>
 
-    <form method="POST" class="logreg" id="login" action="scripts/login.php">
+    <form method="POST" class="logreg" id="login">
 
         <p class="spacing_form">
             <label for="email_log">
@@ -81,7 +85,7 @@
         <input required class="spacing_form input_form" type="password" name="password" id="password_log" size="35" placeholder="heslo" />
 
         <p class="spacing_form">
-            <button class="spacing_form button_form_submit" type="submit" form="login" formaction="">Prihlásiť</button>
+            <button class="spacing_form button_form_submit" type="submit" form="login" formaction="scripts/login.php">Prihlásiť</button>
         </p>
 
     </form>
