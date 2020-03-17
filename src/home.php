f0ae7a27f3d176a749ce49379459fdb701f81317
PetriskoRastislav <petrisko.rastislav@gmail.com>
2020-03-17 19:41:36 +0100

    automatic redirecting of logged/unlogged users

--- src/home.php
+++ src/home.php
@@ -4,6 +4,11 @@ require_once('scripts/scripts.php');
 
 session_start();
 
+if (!isset($_SESSION['id_user'])) {
+    header('Location: index.php');
+    exit();
+}
+
 $page = new Page();
 $page->displayHead( "user", array("styles/style_form-dark.css"));
 $page->displayBodyStart();
--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 
     session_start();
 
-    if(isset($_SESSION['id_user'])){
+    if(isset($_SESSION['id_user'])) {
         header('Location: user.php');
         exit();
     }
