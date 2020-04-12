2ec65187bbdbf5ddf93f3e34c35dc291aa1c6f4c
PetriskoRastislav <petrisko.rastislav@gmail.com>
2020-04-13 00:15:03 +0200

    added back-end script for profile

--- src/scripts/query_users.php
+++ src/scripts/query_users.php
@@ -35,7 +35,7 @@ try {
         while ($statement->fetch()) {
 
             /* will determine whether particular user is still active or not */
-            $time_now = strtotime(date('Y-m-d H:i:s') . '-5 seconds');
+            $time_now = strtotime(date('Y-m-d H:i:s') . '-6 seconds');
             $time_now = date('Y-m-d H:i:s', $time_now);
 
             if ($last_active > $time_now) {
@@ -126,7 +126,7 @@ try {
 
         while ($statement->fetch()){
 
-            $time_now = strtotime(date('Y-m-d H:i:s') . '-5 seconds');
+            $time_now = strtotime(date('Y-m-d H:i:s') . '-6 seconds');
             $time_now = date('Y-m-d H:i:s', $time_now);
 
             if ($last_active > $time_now) {
@@ -195,11 +195,71 @@ try {
 
     }
 
+
+    /* will return information of a profile to the left info panel of profile page */
+    else if ($mode = "get_profile_left_info") {
+
+        $id_user = $_POST['id'];
+
+        $query = "
+            SELECT profile_picture, last_active, gender, location, registered, day_of_birth, month_of_birth, year_of_birth
+            FROM users
+            WHERE id_user = ?";
+        $statement = $db->prepare($query);
+        $statement->bind_param("i", $id);
+        $statement->execute();
+        $statement->bind_result($profile_picture, $last_active, $gender, $location, $registered, $day_of_birth,
+            $month_of_bith, $year_of_birth);
+        $statement->fetch();
+
+        $output = "";
+
+        if ($profile_picture == null) {
+            $output .= "#info_profile_picture|" . "srcPictures/blank-profile-picture-png-8.png";
+        }
+        else {
+            $output .= "#info_profile_picture|" . "usersPictures/" . $profile_picture . "|";
+        }
+
+        $output .= "|#info_last_active|" . process_last_active($last_active);
+
+        $output .= "|#info_gender|" . $gender;
+
+        $output .= "|#info_location|" . $location;
+
+        $output .= "|#info_registered|" . process_to_only_date($registered);
+
+        $output .= "|#info_date_of_birth|";
+
+        if ($day_of_birth != null) {
+            $output .= $day_of_birth . ".";
+        }
+
+        if ($day_of_birth != null || $year_of_birth != null) {
+            $output .= " ";
+        }
+
+        if ($month_of_bith != null) {
+            $output .= process_month($month_of_bith);
+        }
+
+        if ($month_of_bith != null && $year_of_birth != null){
+            $output .= $year_of_birth;
+        }
+
+        print $output;
+
+    }
+
 }
 catch (Exception $ex){
     $ex->getMessage();
     exit();
 }
+
+
+
+/* functions */
 
 
 /* will return number of unseen messages send from a particular user to logged user */
@@ -232,6 +292,117 @@ function get_unseen_messages_notification ($id_user_sender, $id_user_receiver) {
     catch (Exception $ex){
         $ex->getMessage();
         return 0;
+    }
+
+}
+
+
+/* will translate decimal expression of month to a name of a month */
+function process_month ($month) {
+
+    switch ($month) {
+        case "01": return "january";
+        case "02": return "february";
+        case "03": return "march";
+        case "04": return "april";
+        case "05": return "may";
+        case "06": return "jun";
+        case "07": return "july";
+        case "08": return "august";
+        case "09": return "september";
+        case "10": return "october";
+        case "11": return "november";
+        case "12": return "december";
+    }
+
+}
+
+
+/* will return only date from complete timestamp */
+function process_to_only_date ($timestamp) {
+    $date = explode(" ", $timestamp)[0];
+    $date = explode("-", $date);
+
+    $date[1] = process_month($date[1]);
+
+    return  $date[0] . ". " . $date[1] . " " . $date[2];
+}
+
+
+function process_last_active ($timestamp) {
+    $date = explode(" ", $timestamp);
+    $time = explode(":", $date[1]);
+    $date = explode("-", $date[0]);
+
+    $time_limit = strtotime(date('Y-m-d H:i:s') . '-7 days');
+    $time_limit = date('Y-m-d H:i:s', $time_limit);
+
+    if ($timestamp > $time_limit) {
+        return process_to_only_date($timestamp);
+    }
+
+    $time_limit = strtotime(date('Y-m-d H:i:s') . '-1 day');
+    $time_limit = date('Y-m-d H:i:s', $time_limit);
+
+    if ($timestamp > $time_limit) {
+        $day_la = $date[0];
+        $time_limit = explode(" ", $time_limit);
+        $date_l = explode("-", $time_limit[0]);
+        $day_l = $date_l[0];
+
+        $day_la = intval($day_la, 10);
+        $day_l = intval($day_l, 10);
+
+        return ($day_la - $day_l) . " days";
+    }
+
+    $time_limit = strtotime(date('Y-m-d H:i:s') . '-1 hour');
+    $time_limit = date('Y-m-d H:i:s', $time_limit);
+
+    if ($timestamp > $time_limit) {
+        $hour_la = $time[0];
+        $time_limit = explode(" ", $time_limit);
+        $time_l = explode(":", $time_limit[1]);
+        $hour_l = $time_l[0];
+
+        $hour_la = intval($hour_la, 10);
+        $hour_l = intval($hour_l, 10);
+
+        return ($hour_la - $hour_l) . " hours";
+    }
+
+    $time_limit = strtotime(date('Y-m-d H:i:s') . '-1 minute');
+    $time_limit = date('Y-m-d H:i:s', $time_limit);
+
+    if ($timestamp > $time_limit) {
+        $minute_la = $time[1];
+        $time_limit = explode(" ", $time_limit);
+        $time_l = explode(":", $time_limit[1]);
+        $minute_l = $time_l[1];
+
+        $minute_la = intval($minute_la);
+        $minute_l = intval($minute_l);
+
+        return ($minute_la - $minute_l) . " minutes";
+    }
+    else {
+        $second_la = $time[3];
+        $time_limit = explode(" ", $time_limit);
+        $time_l = explode(":", $time_limit[1]);
+        $second_l = $time_l[2];
+
+        $second_la = intval($second_la);
+        $second_l = intval($second_l);
+
+        $second_diff = ($second_la - $second_l);
+
+        if ($second_diff > 6) {
+            return $second_diff . " seconds";
+        }
+        else {
+            return "now";
+        }
+
     }
 
 }
